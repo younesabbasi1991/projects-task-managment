7f122880d8c0cf1a7a662e6e95c0034d44e1a9a2
Younes Abbasi <younesabbasi6@gmail.com>
2025-07-20 07:54:33 +0330

    End of episode 7

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,36 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject.jsx";
 import NoprojectSelected from "./components/NoprojectSelected.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    // undefined ---> not adding a project OR not selecting a project
+    // null ---> if we want to add new project
+
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      };
+    });
+  }
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoprojectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoprojectSelected />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- src/components/NoprojectSelected.jsx
+++ src/components/NoprojectSelected.jsx
@@ -1,13 +1,17 @@
 import noProjectImage from "../assets/no-projects.png";
 import Button from "./Button.jsx";
-export default function NoprojectSelected() {
+export default function NoprojectSelected({ onStartAddProject }) {
   return (
     <div className="mt-24 text-center w-2/3">
-      <img src={noProjectImage} className="w-16 h-16 object-contain mx-auto"/>
-      <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
-      <p className="text-stone-400 mb-4 ">Select a Project or get started with a new one</p>
+      <img src={noProjectImage} className="w-16 h-16 object-contain mx-auto" />
+      <h2 className="text-xl font-bold text-stone-500 my-4">
+        No Project Selected
+      </h2>
+      <p className="text-stone-400 mb-4 ">
+        Select a Project or get started with a new one
+      </p>
       <p className="mb-8">
-        <Button>Create new project</Button>
+        <Button onClick={onStartAddProject}> Create new project </Button>
       </p>
     </div>
   );
